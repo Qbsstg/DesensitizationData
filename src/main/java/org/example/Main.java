d466ff89dca9097becf86a24b212c95c04ea38a4
Qbsstg <wuhao5207@gmail.com>
2024-02-28 23:24:56 +0800

    初始化

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,58 @@
 package org.example;
 
-//TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
-// 点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.extra.tokenizer.Result;
+import cn.hutool.extra.tokenizer.Word;
+import cn.hutool.extra.tokenizer.engine.jieba.JiebaEngine;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP 当文本光标位于高亮显示的文本处时按 <shortcut actionId="ShowIntentionActions"/>
-        // 查看 IntelliJ IDEA 建议如何修正。
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP 按 <shortcut actionId="Debug"/> 开始调试代码。我们已经设置了一个 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 断点
-            // 但您始终可以通过按 <shortcut actionId="ToggleLineBreakpoint"/> 添加更多断点。
-            System.out.println("i = " + i);
+        // 分词器
+        JiebaEngine engine = new JiebaEngine();
+        Result r = engine.parse("我来到北京清华大学");
+        Word next = r.next();
+        String s = CollUtil.join((Iterable<Word>) r, " ");
+
+        List<String> list = Arrays.asList("a", "b", "c");
+
+
+        System.out.println(s);
+        System.out.println(replaceRandomElements(list));
+    }
+
+
+    public static String replaceRandomElements(List<String> list) {
+        int replaceCount = getReplaceCount(list.size());
+        // Random rand = new Random();
+
+        List<Integer> indicesToReplace = IntStream.range(0, list.size())
+                .boxed()
+                .collect(Collectors.toList());
+        Collections.shuffle(indicesToReplace);
+
+        for (int i = 0; i < replaceCount; i++) {
+            list.set(indicesToReplace.get(i), "*");
+        }
+
+        return String.join(" ", list);
+    }
+
+    private static int getReplaceCount(int size) {
+        if (size > 10) {
+            return 3;
+        } else if (size > 5) {
+            return 2;
+        } else {
+            return 1;
         }
     }
+
+
 }
